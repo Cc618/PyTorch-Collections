6ff14cc7edd5832c10586d9e6bfcb94b5526d9d5
Celian Raimbault <celian.raimbault@epita.fr>
2020-03-05 15:28:15 +0100

    Updated obj_detect (colors)

--- src/obj_detect.py
+++ src/obj_detect.py
@@ -1,3 +1,5 @@
+# Cats are red, dogs are blue
+
 import os
 import torch as T
 from torch import nn
@@ -7,7 +9,52 @@ from torchvision.datasets import ImageFolder
 from torchvision import transforms
 import numpy as np
 import PIL.Image as im
-from utils import dataset_dir, models_dir, img_load, img_show, stack_show
+from utils import dataset_dir, models_dir, img_load, img_show, stack_show, img_div, img_undiv
+
+
+def colorize(img, class_ratio, alpha=1):
+    '''
+        Colorizes in blue / red the image with the ratio
+        that this image belongs to a cat / dog
+        (.5 = unknown, 0/1 = cat/dog at 100% chance)
+    - alpha : Opacity of the color
+    '''
+    global red_img, blue_img
+
+    if class_ratio > .5:
+        # [0, 1], 1 = 100% chance
+        ratio = (class_ratio - .5) * 2
+        ratio *= alpha
+
+        return img * (1 - ratio) + blue_img * ratio
+    
+    # [0, 1], 1 = 100% chance
+    ratio = (.5 - class_ratio) * 2
+    ratio *= alpha
+
+    return img * (1 - ratio) + red_img * ratio
+
+
+def detect_colorize(net, img, div_width, div_height, alpha=.5):
+    '''
+        Divides an image into subdivisions, detects and colorizes each
+    subdivision with the color associated to the prediction and
+    reconstructs the image with colors
+    - img : Tensor
+    - return : Tensor
+    '''
+    _, crop_width, _ = img.size()
+    crop_width //= div_width
+    
+    # Divide
+    divs = img_div(img, div_width, div_height)
+    
+    # Classify
+    for i in range(len(divs)):
+        divs[i] = colorize(divs[i], net(divs[i].unsqueeze(0)), alpha=alpha)
+
+    # Reconstruct
+    return img_undiv(divs, crop_width, device)
 
 
 class Net(Module):
@@ -50,7 +97,7 @@ class Net(Module):
 # Params
 im_width = 128
 im_height = 128
-epochs = 10
+epochs = 1
 batch_size = 100
 learning_rate = .0002
 train_or_test = 'test'
@@ -62,16 +109,19 @@ device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
 # Data
 test_dataset_path = dataset_dir + '/cats_dogs/test'
 train_dataset_path = dataset_dir + '/cats_dogs/train'
+blue_img = T.tensor([0, 0, 1], dtype=T.float, device=device).reshape(3, 1, 1).repeat([1, im_width, im_height])
+red_img = T.tensor([1, 0, 0], dtype=T.float, device=device).reshape(3, 1, 1).repeat([1, im_width, im_height])
 
 # Network
 net = Net(im_width, im_height)
 net.to(device)
 
+# Load
+if os.path.exists(path):
+    net.load_state_dict(T.load(path))
+    print('Model loaded')
+
 if train_or_test == 'train':
-    # Load
-    if os.path.exists(path):
-        net.load_state_dict(T.load(path))
-        print('Model loaded')
 
     # Dataset
     dataset = ImageFolder(
@@ -122,10 +172,7 @@ if train_or_test == 'train':
 
     print('Model trained and saved')
 
-else:
-    # Load
-    net.load_state_dict(T.load(path))
-
+elif train_or_test == 'test':
     # Dataset
     dataset = ImageFolder(
         root=test_dataset_path,
@@ -151,3 +198,11 @@ else:
                     ok += 1
 
     print(f'Accuracy : {ok / len(dataset) * 100:.1f} %')
+
+elif train_or_test == 'misc':
+    # Show image with colors
+    tst_path = './data/cats_dogs.jpg'
+    img = im.open(tst_path)
+    img = transforms.Compose([transforms.Resize((128 * 6, 128 * 8)), transforms.ToTensor()])(img).to(device)
+
+    img_show(detect_colorize(net, img, im_width, im_height))
